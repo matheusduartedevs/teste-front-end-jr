ad7c48f9d0b255243280a71f901246fb583f388b
unknown <matheus.duartesilva42@gmail.com>
2024-06-05 20:12:57 -0300

    feat: implementação do footer

--- teste-frontend-econverse/src/App.tsx
+++ teste-frontend-econverse/src/App.tsx
@@ -1,5 +1,6 @@
 import Banner from "./components/Banner/Banner"
 import Categorias from "./components/Categorias/Categorias"
+import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import NavegueMarcas from "./components/NavegueMarcas/NavegueMarcas"
 import Parceiros from "./components/Parceiros/Parceiros"
@@ -16,6 +17,7 @@ const App = () => {
       <Parceiros />
       <ProdutosRelacionados />
       <NavegueMarcas />
+      <Footer />
     </>
   )
 }
